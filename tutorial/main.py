0a4907f0d2eb957f7ebe1c5277ed25817fb67541
Farid Karimli <karimli.farid@icloud.com>
2024-03-17 10:51:18 -0400

    Initial commit

--- tutorial/main.py
+++ tutorial/main.py
@@ -15,6 +15,10 @@ import tensorflow as tf
 import keras
 from keras import layers
 from loader import list_files_per_class, get_class, get_files_per_class, download_ufc_101_subset, FrameGenerator
+from model import Conv2Plus1D, ResidualMain, Project, add_residual_block, ResizeVideo
+
+# check if dataset exists
+# if not pathlib.Path('./UCF101_subset').exists():
 
 URL = 'https://storage.googleapis.com/thumos14_files/UCF101_videos.zip'
 download_dir = pathlib.Path('./UCF101_subset/')
@@ -22,6 +26,12 @@ subset_paths = download_ufc_101_subset(URL,
                                        num_classes=10,
                                        splits={"train": 30, "val": 10, "test": 10},
                                        download_dir=download_dir)
+# else:
+#     subset_paths = {
+#         "train": get_files_per_class('UCF101_subset/train'),
+#         "val": get_files_per_class('UCF101_subset/val'),
+#         "test": get_files_per_class('UCF101_subset/test')
+#     }
 
 n_frames = 10
 batch_size = 8
@@ -43,3 +53,176 @@ test_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['test'], n_
                                          output_signature=output_signature)
 
 test_ds = test_ds.batch(batch_size)
+
+HEIGHT = 224
+WIDTH = 224
+
+input_shape = (None, 10, HEIGHT, WIDTH, 3)
+input = layers.Input(shape=(input_shape[1:]))
+x = input
+
+x = Conv2Plus1D(filters=16, kernel_size=(3, 7, 7), padding='same')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = ResizeVideo(HEIGHT // 2, WIDTH // 2)(x)
+
+# Block 1
+x = add_residual_block(x, 16, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 4, WIDTH // 4)(x)
+
+# Block 2
+x = add_residual_block(x, 32, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 8, WIDTH // 8)(x)
+
+# Block 3
+x = add_residual_block(x, 64, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 16, WIDTH // 16)(x)
+
+# Block 4
+x = add_residual_block(x, 128, (3, 3, 3))
+
+x = layers.GlobalAveragePooling3D()(x)
+x = layers.Flatten()(x)
+x = layers.Dense(10)(x)
+
+print("Defining the model")
+
+model = keras.Model(input, x)
+
+frames, label = next(iter(train_ds))
+model.build(frames)
+
+keras.utils.plot_model(model, expand_nested=True, dpi=60, show_shapes=True)
+
+print("Model defined")
+
+model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+              metrics=['accuracy'])
+
+history = model.fit(x=train_ds,
+                    epochs=50,
+                    validation_data=val_ds)
+
+
+def plot_history(history):
+    """
+      Plotting training and validation learning curves.
+
+      Args:
+        history: model history with all the metric measures
+    """
+    fig, (ax1, ax2) = plt.subplots(2)
+
+    fig.set_size_inches(18.5, 10.5)
+
+    # Plot loss
+    ax1.set_title('Loss')
+    ax1.plot(history.history['loss'], label='train')
+    ax1.plot(history.history['val_loss'], label='test')
+    ax1.set_ylabel('Loss')
+
+    # Determine upper bound of y-axis
+    max_loss = max(history.history['loss'] + history.history['val_loss'])
+
+    ax1.set_ylim([0, np.ceil(max_loss)])
+    ax1.set_xlabel('Epoch')
+    ax1.legend(['Train', 'Validation'])
+
+    # Plot accuracy
+    ax2.set_title('Accuracy')
+    ax2.plot(history.history['accuracy'], label='train')
+    ax2.plot(history.history['val_accuracy'], label='test')
+    ax2.set_ylabel('Accuracy')
+    ax2.set_ylim([0, 1])
+    ax2.set_xlabel('Epoch')
+    ax2.legend(['Train', 'Validation'])
+
+    plt.show()
+
+
+plot_history(history)
+
+print("Evaluating the model on the test set")
+results = model.evaluate(test_ds, return_dict=True)
+print("Test set accuracy", results['accuracy'])
+
+
+def get_actual_predicted_labels(dataset):
+    """
+      Create a list of actual ground truth values and the predictions from the model.
+
+      Args:
+        dataset: An iterable data structure, such as a TensorFlow Dataset, with features and labels.
+
+      Return:
+        Ground truth and predicted values for a particular dataset.
+    """
+    actual = [labels for _, labels in dataset.unbatch()]
+    predicted = model.predict(dataset)
+
+    actual = tf.stack(actual, axis=0)
+    predicted = tf.concat(predicted, axis=0)
+    predicted = tf.argmax(predicted, axis=1)
+
+    return actual, predicted
+
+
+def plot_confusion_matrix(actual, predicted, labels, ds_type):
+    cm = tf.math.confusion_matrix(actual, predicted)
+    ax = sns.heatmap(cm, annot=True, fmt='g')
+    sns.set(rc={'figure.figsize': (12, 12)})
+    sns.set(font_scale=1.4)
+    ax.set_title('Confusion matrix of action recognition for ' + ds_type)
+    ax.set_xlabel('Predicted Action')
+    ax.set_ylabel('Actual Action')
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+
+
+fg = FrameGenerator(subset_paths['train'], n_frames, training=True)
+labels = list(fg.class_ids_for_name.keys())
+
+actual, predicted = get_actual_predicted_labels(train_ds)
+plot_confusion_matrix(actual, predicted, labels, 'training')
+
+actual, predicted = get_actual_predicted_labels(test_ds)
+plot_confusion_matrix(actual, predicted, labels, 'test')
+
+
+def calculate_classification_metrics(y_actual, y_pred, labels):
+    """
+      Calculate the precision and recall of a classification model using the ground truth and
+      predicted values.
+
+      Args:
+        y_actual: Ground truth labels.
+        y_pred: Predicted labels.
+        labels: List of classification labels.
+
+      Return:
+        Precision and recall measures.
+    """
+    cm = tf.math.confusion_matrix(y_actual, y_pred)
+    tp = np.diag(cm)  # Diagonal represents true positives
+    precision = dict()
+    recall = dict()
+    for i in range(len(labels)):
+        col = cm[:, i]
+        fp = np.sum(col) - tp[i]  # Sum of column minus true positive is false negative
+
+        row = cm[i, :]
+        fn = np.sum(row) - tp[i]  # Sum of row minus true positive, is false negative
+
+        precision[labels[i]] = tp[i] / (tp[i] + fp)  # Precision
+
+        recall[labels[i]] = tp[i] / (tp[i] + fn)  # Recall
+
+    return precision, recall
+
+
+precision, recall = calculate_classification_metrics(actual, predicted, labels)  # Test dataset
+print("Precision", precision)
+print("Recall", recall)
